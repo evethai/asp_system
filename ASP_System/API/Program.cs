9c275b5c8e2010e245414abf7447ed406bfe7579
evethai <117005361+evethai@users.noreply.github.com>
2024-03-16 08:41:58 +0700

    not change

--- ASP_System/API/Program.cs
+++ ASP_System/API/Program.cs
@@ -96,6 +96,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();

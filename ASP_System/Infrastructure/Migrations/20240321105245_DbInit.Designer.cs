b802be5fbac52c169867a273fe5bac2bb13930c0
Nguyễn Tấn Đạt <dat.nt271102@gmail.com>
2024-03-21 18:35:12 +0700

    save before merge

--- ASP_System/Infrastructure/Migrations/20240321105245_DbInit.Designer.cs
+++ ASP_System/Infrastructure/Migrations/20240321105245_DbInit.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240318065644_DbInit")]
+    [Migration("20240321105245_DbInit")]
     partial class DbInit
     {
         /// <inheritdoc />
